53df58a29c3932ca6c013c4e7197d379cd238523
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-04 18:56:34 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Sportello
+=========
+
+Bowling kata in Clojure from Scratch.  Aaron Bedra cghallenged me to do it in 25 lines, so let's rock.
---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject sportello "1.0.0-SNAPSHOT"
+  :description "bowling kata"
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]])
