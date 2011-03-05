cb59a32e40d60fb9581b042d8d1281eac28734d1
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-05 16:45:57 -0500

    improved api to bowling game by using defnk

--- src/bowling/game.clj
+++ src/bowling/game.clj
@@ -1,10 +1,21 @@
-(ns bowling.game)
+(ns bowling.game
+  (:use clojure.contrib.def))
   
 (defn start-game []
   )  
   
-(defn roll [pins-hit]
-   )
+(defn- score-roll [pins-hit]
+  )  
+  
+(defnk roll [pins-hit :times 1]
+   (doseq [i (repeat times pins-hit)]
+    (score-roll i))) 
+   
+(defn roll-spare [] 
+  (roll 5 :times 2))
+
+(defn roll-strike []
+  (roll 10))
    
 (defn score-game []
   0)
--- test/bowling/test/game.clj
+++ test/bowling/test/game.clj
@@ -1,32 +1,22 @@
 (ns bowling.test.game
   (:use bowling.game)
   (:use midje.sweet))
-
-(defn roll-many [pins-hit num-rolls]
-  (doseq [i (repeat num-rolls pins-hit)]
-    (roll i)))   
-   
-(defn- roll-spare [] 
-  (roll-many 5 2))
-
-(defn- roll-strike []
-  (roll 10))
 	
 (fact "scores 0 when no pins knocked down for whole game"
   (start-game)
-  (roll-many 0 20)
+  (roll 0 :times 20)
   (score-game) => 0)
 
 (fact "score 20 when all rolls of one pin"
   (start-game)
-  (roll-many 1 20)
+  (roll 1 :times 20)
   (score-game) => 20)
 
 (fact "counts the roll after a spare twice"
   (start-game)
   (roll-spare)
   (roll 3)
-  (roll-many 0 16)
+  (roll 0 :times 16)
   (score-game) => 16)
 
 (fact "counts the two rolls afer a strike twice"
@@ -34,17 +24,17 @@
   (roll-strike)
   (roll 3)
   (roll 4)
-  (roll-many 0 16)
+  (roll 0 :times 16)
   (score-game) => 24)
 
 (fact "scores 300 for a perfect game: 12 strikes"
   (start-game)
-  (roll-many 10 12)
+  (roll 10 :times 12)
   (score-game) => 300)
 
 (fact "scores 150 for all fives (a spare each round)"
   (start-game)
-  (roll-many 5 21)
+  (roll 5 :times 21)
   (score-game) => 150)
 
 	
