2f1df12d7fae7db58aa903ded588932b7251a18b
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-05 17:18:45 -0500

    refactored frames to be a vector of 10 vectors, each one holding the number of pins hit on that particular roll: in preparation for implementing the spare fact

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 Sportello
 =========
 
-Bowling kata in Clojure from Scratch.  Aaron Bedra cghallenged me to do it in 25 lines, so let's rock.
+Bowling kata in Clojure from Scratch.  Aaron Bedra challenged me to do it in 25 lines, so let's rock.
--- src/bowling/game.clj
+++ src/bowling/game.clj
@@ -1,18 +1,28 @@
 (ns bowling.game
   (:use clojure.contrib.def))
-
-(defvar- the-game (atom 0))
+  
+(defvar- sum (partial reduce +))  
+  
+(defn- replace-last [seq item]
+  (conj (vec (drop-last 1 seq)) item))
+  
+(defvar- frames (atom [[] [] [] [] [] [] [] [] [] []]))
   
 (defn start-game []
   )
   
+(defn add-roll [frames pins-hit]  
+  (if (= 2 (count (last frames)))
+      (conj frames [pins-hit])
+	  (replace-last frames (conj (last frames) pins-hit))))
+  
 (defn- score-roll [pins-hit]
-  (swap! the-game + pins-hit))	
+  (swap! frames add-roll pins-hit))	
   
 (defnk roll [pins-hit :times 1]
    (doseq [i (repeat times pins-hit)]
     (score-roll i))) 
    
 (defn score-game []
-  @the-game)
+  (sum (map sum @frames)))
    
