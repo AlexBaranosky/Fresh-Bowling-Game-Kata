ded64478b393fa77a45f6357d35df5cd7bb053b0
AlexBaranosky <alexander.baranosky@gmail.com>
2011-03-07 19:08:36 -0500

    kept same api, but changed internals to represent the data as a vector or rolls, instead of a vector of vectors, as per Aaron Bedra's suggestions

--- src/bowling/game.clj
+++ src/bowling/game.clj
@@ -3,39 +3,37 @@
   
 (defvar- sum (partial reduce +))
   
-(defvar- frames (atom [[]]))
+(defvar- rolls (atom []))
 
-(defn- all-pins-down-and-on-nth-roll? [nth-roll frame]
-  (and (= nth-roll (count frame)) (= 10 (sum frame))))   
-	
-(defvar- spare? (partial all-pins-down-and-on-nth-roll? 2))
+(defn spare? [rolls]
+  (= 10 (sum (take 2 rolls))))
 
-(defvar- strike? (partial all-pins-down-and-on-nth-roll? 1)) 
-  
-(defn- add-roll-to-frames [frames pins-hit]        
-  (if (and (not= 10 (count frames)) 
-           (or (= 2 (count (last frames))) (strike? (last frames))))
-      (conj frames [pins-hit])
-	  (assoc frames (dec (count frames)) (conj (last frames) pins-hit)))) 
-  
-(defn- score-frame [frame next-frame third-frame]
-  (cond (and (strike? frame) next-frame) 
-		(+ (sum frame) (first next-frame) (if (= 1 (count next-frame)) (first third-frame) (second next-frame)))
-        (and (spare? frame) next-frame)  
-		(+ (sum frame) (first next-frame))
-        :else 
-		(sum frame)))  
-
+(defn strike? [rolls]
+  (= 10 (first rolls)))   
+   
+(defn rolls-to-advance [rolls frame]
+	(if (= 10 frame)
+	    (if (or (spare? rolls) (strike? rolls)) 3 2)
+        (if (strike? rolls) 1 2)))		
+		     
+(defn items-in-frame [rolls]
+  (cond (strike? rolls) 3
+        (spare? rolls) 3
+        :else 2))
+   
 (defn start-game! []
   (prn "starting game...")
-  (reset! frames [[]]))		
+  (reset! rolls []))		
 		
 (defnk roll! [pins-hit :times 1]
   (dotimes [i times] 
-   (swap! frames add-roll-to-frames pins-hit))) 
+   (swap! rolls conj pins-hit))) 
    
 (defn score-game []
-  (prn "frames: " @frames)
-  (let [next-frames (concat (rest @frames) [nil])
-        third-frames (concat (rest next-frames) [nil])]
-    (sum (map score-frame @frames next-frames third-frames))))
+  (prn "rolls: " @rolls)
+  (loop [total 0 frame 1 rolls @rolls]
+    (if (not (empty? rolls))
+      (recur (+ total (sum (take (items-in-frame rolls) rolls)))
+             (inc frame)
+             (drop (rolls-to-advance rolls frame) rolls))
+      total)))
